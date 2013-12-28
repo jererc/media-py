6a4249074eac00a80ef10ea5333170b203b2736c
Jerome Clerc <jererc@gmail.com>
2013-12-28 01:31:51 +0100

    add google drive query

--- media/workers/query.py
+++ media/workers/query.py
@@ -1,31 +1,113 @@
+import httplib2
 import imaplib
 from email import message_from_string
+import time
+from calendar import timegm
+from urlparse import urlparse
 import re
 import logging
 
+import dateutil.parser
+
+from lxml import html
+
+logging.getLogger('apiclient').setLevel(logging.ERROR)
+logging.getLogger('oauth2client').setLevel(logging.ERROR)
+
+from apiclient import errors
+from apiclient.discovery import build
+from apiclient.http import MediaInMemoryUpload
+from oauth2client.client import Credentials, AccessTokenRefreshError
+
 from systools.system import loop, timeout, timer
 
-from filetools.title import clean
+from transfer import Transfer
 
-from mediacore.model.search import Search, add_movies, add_music
+from mediacore.model.search import Search
 from mediacore.model.settings import Settings
+from mediacore.utils.query import get_searches, QueryError
 
 
-CAT_DEF = {
-    'anime': re.compile(r'\banime\b', re.I),
-    'apps': re.compile(r'\bapplications?\b', re.I),
-    'books': re.compile(r'\be?books?\b', re.I),
-    'games': re.compile(r'\bgames?\b', re.I),
-    'movies': re.compile(r'\b(movies?|video)\b', re.I),
-    'music': re.compile(r'\b(audio|music)\b', re.I),
-    'tv': re.compile(r'\b(tv|tv\s*shows?)\b', re.I),
-    }
-RE_ARTIST = {
-    'movies': re.compile(r'\b(artist|director|actor)\b', re.I),
-    'music': re.compile(r'\b(artist|band)\b', re.I),
-    }
+MODIFIED_DELTA = 30     # drive file modified delta in seconds
 
 logger = logging.getLogger(__name__)
+
+
+class DriveError(Exception): pass
+
+
+class DriveClient(object):
+
+    def __init__(self, credentials):
+        credentials_ = Credentials.new_from_json(credentials)
+        http = httplib2.Http()
+        http = credentials_.authorize(http)
+        self.service = build('drive', 'v2', http=http)
+
+    def get_file_by_id(self, file_id):
+        return self.service.files().get(fileId=file_id).execute()
+
+    def get_file_by_title(self, title):
+        res = self.service.files().list(q="title='%s'" % title).execute()
+        return res['items'][0] if res['items'] else None
+
+    def get_file_content(self, file, mime_type='text/html'):
+        url = file.get('exportLinks', {}).get(mime_type)
+        if not url:
+            raise DriveError('missing file url for mime type %s: %s' % (mime_type, file))
+        resp, content = self.service._http.request(url)
+        if resp.status != 200:
+            raise DriveError('google api error: %s' % resp)
+        return content.decode('utf-8-sig')
+
+    def set_file_content(self, file, body, mime_type='text/html'):
+        media = MediaInMemoryUpload(body, mimetype=mime_type)
+        try:
+            return self.service.files().update(fileId=file['id'],
+                    newRevision=True, media_body=media).execute()
+        except errors.HttpError, e:
+            raise DriveError('google api error: %s' % str(e))
+
+
+class FileLine(object):
+
+    def __init__(self, element):
+        self.element = element
+        self.spans = self.element.cssselect('span')
+
+    def _get_line(self):
+        res = ''
+        for el in self.spans:
+            links = el.cssselect('a')
+            res += links[0].get('href') if links else el.text
+        return res
+
+    def _get_element(self):
+        return html.tostring(self.element)
+
+    def _get_sub_pattern(self):
+        return re.sub(r'class\\=\\"[^\"]*\\"', r'class="([^\"]*)"',
+                re.escape(self._get_element()))
+
+    def _replace(self, matchobj):
+        self.group_num += 1
+        return 'class="\\%d"' % self.group_num
+
+    def _get_sub_repl(self):
+        self.group_num = 0
+        return re.sub(r'class="([^\"]*)"', self._replace, self._get_element())
+
+    def get_query(self):
+        if self.spans:
+            begin = self.spans[0].text
+            if begin and begin.startswith('?'):
+                return self._get_line().lstrip('?').strip()
+
+    def get_patterns(self):
+        pattern = self._get_sub_pattern()
+        self.spans[0].text = self.spans[0].text.lstrip('?')
+        repl = self._get_sub_repl()
+        return pattern, repl
 
 
 class GmailClient(object):
@@ -67,62 +149,87 @@ class GmailClient(object):
         typ, response = self.client.store(num, '+FLAGS', r'(\Deleted)')
 
 
-def get_category_info(val):
-    for category, re_cat in CAT_DEF.items():
-        if re_cat.search(val):
-            re_ = RE_ARTIST.get(category)
-            return category, re_ and re_.search(val)
-    return None, False
+def check_modified(date, delta):
+    modified = dateutil.parser.parse(date)
+    elapsed = int(time.time()) - timegm(modified.utctimetuple())
+    return elapsed > delta
 
 def process_query(query):
-    parts = [v.strip() for v in query.split(',')]
-    if len(parts) < 2:
-        return False
-    category, is_artist = get_category_info(clean(parts.pop(0)))
-    if category is None:
-        return False
-    name = clean(parts.pop(0), 1)
-    if not name:
-        return False
-    artist = name if is_artist else None
-
-    logger.info('processing query "%s"', query)
-
-    langs = Settings.get_settings('media_langs').get(category, [])
-    search = {
-        'name': name,
-        'category': category,
-        'mode': 'once',
-        'langs': langs,
-        }
-
-    if category == 'music':
-        if not parts:
-            artist = name
-        if artist:
-            return add_music(artist)
-
-        search['album'] = clean(parts.pop(0), 1)
-        if not search['album']:
+    if urlparse(query).scheme:
+        dst = Settings.get_settings('paths')['finished_download']
+        try:
+            Transfer.add(query, dst)
+        except Exception, e:
+            logger.error('failed to create transfer for %s: %s', query, str(e))
             return False
 
-    elif category == 'movies':
-        if artist:
-            return add_movies(artist, langs=langs)
+    else:
+        try:
+            searches = get_searches(query)
+        except QueryError, e:
+            logger.info(str(e))
+            return False
 
-    elif category in ('tv', 'anime'):
-        search['mode'] = 'inc'
-        search['season'] = 1
-        search['episode'] = 1
+        if not searches:
+            logger.info('no result for query "%s', query)
+        else:
+            for search in searches:
+                if Search.add(**search):
+                    logger.info('created search %s', search)
 
-    if Search.add(**search):
-        logger.info('created %s search %s', category, search)
     return True
 
-@loop(60)
+def process_file_queries(body):
+    '''Process the file queries and return a list of patterns and replacement strings.
+    '''
+    res = []
+    tree = html.fromstring(body)
+    for element in tree.cssselect('body p'):
+        line = FileLine(element)
+        query = line.get_query()
+        if query is None:
+            continue
+        if not process_query(query):
+            continue
+        res.append(line.get_patterns())
+
+    return res
+
 @timeout(minutes=10)
 @timer(60)
-def run():
+def process_drive():
+    file_title = Settings.get_settings('google_drive').get('file_title')
+    if not file_title:
+        return
+    credentials = Settings.get_settings('google_api_credentials').get('credentials')
+    if not credentials:
+        return
+
+    drive = DriveClient(credentials)
+    try:
+        file_ = drive.get_file_by_title(file_title)
+        if not file_:
+            return
+        if not check_modified(file_['modifiedDate'], delta=MODIFIED_DELTA):
+            return
+
+        body = drive.get_file_content(file_, mime_type='text/html')
+        patterns = process_file_queries(body)
+        if patterns:
+            body = drive.get_file_content(file_, mime_type='text/html')
+            for pattern, repl in patterns:
+                body = re.sub(pattern, repl, body, 1)
+            drive.set_file_content(file_, body=body, mime_type='text/html')
+
+    except AccessTokenRefreshError:
+        Settings.set_settings('google_api_credentials', {'credentials': None})
+        logger.error('revoked or expired credentials')
+    except DriveError, e:
+        logger.error(str(e))
+
+@timeout(minutes=10)
+@timer(60)
+def process_email():
     email_ = Settings.get_settings('email')
     if not email_.get('host') \
             or not email_.get('port') \
@@ -138,3 +245,8 @@ def run():
     for message in client.iter_messages(email_['from_email']):
         if process_query(message['subject']):
             client.delete(message['num'])
+
+@loop(60)
+def run():
+    process_drive()
+    process_email()
